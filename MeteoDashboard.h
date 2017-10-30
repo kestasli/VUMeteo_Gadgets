8fe22a55c562ddbcc965b6834cc6cb09309faffe
kestasli <k.liaugminas@gmail.com>
2017-10-30 23:51:38 +0200

    Class names changed and bar number added

--- MeteoDashboard.h
+++ MeteoDashboard.h
@@ -1,32 +1,22 @@
-class Bar{
+class NumberIndicator{
 public:
-  Bar();
-  Bar(Adafruit_ILI9341 &dsp, int x, int y);
-  void setValue(float value);
-  void setTest(int value);
-private:
-  Adafruit_ILI9341 *tft;
-  int W;
-  int H;
-  int x;
-  int y;
-  int barWidth = 10, barSpace = 2, barMaxHeight = 42;
-  int valueCount = 8, textRectHeight = 18;
-  int averageover = 1; //average over some measures
-  int measureslist[20];
-};
-
-class Temp{
-public:
-  Temp();
-  Temp(Adafruit_ILI9341 &dsp);
+  NumberIndicator();
+  NumberIndicator(Adafruit_ILI9341 &dsp);
+  NumberIndicator(Adafruit_ILI9341 &dsp, int x, int y);
+  //Temp(Adafruit_ILI9341 &dsp, int x, int y, GFXfont *font);
+  NumberIndicator(Adafruit_ILI9341 &dsp, int x, int y, GFXfont *font, int size);
+  int getTextWidth();
+  int getTextHeight();
   void set(float value);
-  void set1(float value);
 private:
   char* deblank(char* origstring);
   Adafruit_ILI9341 *tft;
-  int W, H; //display width and height
+  GFXfont *font;
+  int W, H, x, y; //display width and height
+  int size; //font size
   float oldvalue = 99.9, value = 0;
+  int16_t  txtX, txtY; //current coordinates of text boudary
+  uint16_t txtW, txtH; //current coordinates of text boudary
   int16_t  o_txtX = 0, o_txtY = 0; //keep old coordinates of text boundary
   uint16_t o_txtW = 0, o_txtH = 0; //keep old koordinates of text boundary
 };
@@ -38,6 +28,9 @@ public:
   void set(float value);
 private:
   Adafruit_ILI9341 *tft;
+  GFXfont *font;
+  NumberIndicator level_value;
+  int fontHeight, fontSize;
   int W, H, x, y;
   int maxvalue = 10;
   int barWidth = 10, barSpace = 2;
--- MeteoDashbord.cpp
+++ MeteoDashbord.cpp
@@ -1,31 +1,50 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <MeteoDashboard.h>
-#include <Fonts/FreeSansBold24pt7b.h>
+#include <Fonts/FreeSansBold12pt7b.h>
+//#include <Fonts/FreeSansBold24pt7b.h>
 //#include <Fonts/FreeSansBold56pt7b.h>
 //#include <Fonts/FreeSansBold44pt7b.h>
 
-Temp::Temp(){
+NumberIndicator::NumberIndicator(){
 }
 
-Temp::Temp(Adafruit_ILI9341 &dsp){
+NumberIndicator::NumberIndicator(Adafruit_ILI9341 &dsp){
+  tft = &dsp;
+}
+
+NumberIndicator::NumberIndicator(Adafruit_ILI9341 &dsp, int x0, int y0){
   tft = &dsp;
   W = tft->width();
   H = tft->height();
+  x = x0, y = y0;
+  //tft->setFont(&FreeSansBold24pt7b);
+  tft->setTextSize(2);
 }
 
-void Temp::set(float value){
+NumberIndicator::NumberIndicator(Adafruit_ILI9341 &dsp, int x0, int y0, GFXfont *font0, int size0){
+  tft = &dsp;
+  W = tft->width();
+  H = tft->height();
+  x = x0, y = y0;
+  font = font0;
+  size = size0;
+  tft->setFont(font);
+  tft->setTextSize(size);
+}
+
+void NumberIndicator::set(float value){
   tft->setTextWrap(false);
   tft->setTextColor(ILI9341_WHITE);
-  tft->setFont(&FreeSansBold24pt7b);
+  tft->setFont(font);
+  tft->setTextSize(size);
   //tft->setFont(&FreeSansBold56pt7b);
   //tft->setFont(&FreeSansBold44pt7b);
-  tft->setTextSize(2);
+  //tft->setTextSize(2);
 
-  int cursorX;
-  int cursorY;
-  int16_t  txtX, txtY;
-  uint16_t txtW, txtH;
+  int cursorX = x;
+  int cursorY = y;
+
   //char value_str[] = "-00.0";
   char value_str[] = "-00.0";
 
@@ -37,8 +56,9 @@ void Temp::set(float value){
   dtostrf(value, 5, 1, value_str);
   char* value_nospc = deblank(value_str);
   tft->getTextBounds(value_nospc, 0, 0, &txtX, &txtY, &txtW, &txtH);
-  cursorX = (W - txtW)/2;
-  cursorY = txtH + 30;
+  //tft->drawCircle(x + txtW, y, 8, ILI9341_WHITE);
+  //cursorX = (W - txtW)/2;
+  //cursorY = txtH + 30;
 
   tft->setCursor(cursorX, cursorY);
   tft->print(value_nospc);
@@ -47,7 +67,7 @@ void Temp::set(float value){
 
 }
 
-char* Temp::deblank(char* input)
+char* NumberIndicator::deblank(char* input)
 {
     int i,j;
     char *output=input;
@@ -62,72 +82,6 @@ char* Temp::deblank(char* input)
     return output;
 }
 
-
-void Temp::set1(float value){
-  tft->setTextWrap(false);
-  tft->setTextSize(2);
-
-  int cursorX;
-  int cursorY;
-  int16_t  txtX, txtY;
-  uint16_t txtW, txtH;
-  char value_str[] = "-00.0";
-
-  cursorX = 10;
-  cursorY = 120;
-
-  if (oldvalue != 99.9) {
-    tft->setTextColor(ILI9341_BLACK);
-    tft->setFont(&FreeSansBold24pt7b);
-    //tft->setFont(&FreeSansBold56pt7b);
-    //tft->setFont(&FreeSansBold44pt7b);
-    tft->setCursor(cursorX, cursorY);
-    tft->print(oldvalue, 1);
-  }
-
-  tft->setTextColor(ILI9341_WHITE);
-  tft->setFont(&FreeSansBold24pt7b);
-  //tft->setFont(&FreeSansBold56pt7b);
-  //tft->setFont(&FreeSansBold44pt7b);
-  tft->setCursor(cursorX, cursorY);
-  tft->print(value, 1);
-  //get values of current placement into "old coordinates"
-  oldvalue = value;
-}
-
-
-Bar::Bar(){
-}
-
-Bar::Bar(Adafruit_ILI9341 &dsp, int x0, int y0){
-  x = x0;
-  y = y0;
-  tft = &dsp;
-  tft->fillRect(x, y + barMaxHeight + 1, valueCount * (barWidth + barSpace) - barSpace, textRectHeight, ILI9341_NAVY);
-}
-
-void Bar::setValue(float windspeed){
-  tft->fillRect(x, y, (barWidth + barSpace) * valueCount, barMaxHeight + textRectHeight + 1, ILI9341_BLACK);
-  tft->fillRect(x, y + barMaxHeight + 1, valueCount * (barWidth + barSpace) - barSpace, textRectHeight, ILI9341_NAVY);
-  tft->setFont();
-  tft->setTextSize(2);
-  tft->setTextColor(ILI9341_WHITE);
-  //pataisyti, kad Y koordinate butu isskaiciuojama automatiskai
-  tft->setCursor(x + 2, y + barMaxHeight + 3);
-  tft->print(windspeed, 1);
-  tft->print("m/s");
-
-  int barNumber = (int)(windspeed + 0.5);
-
-  if (barNumber >= valueCount) {
-    barNumber = valueCount;
-  }
-
-  for (int i = 0 ; i < barNumber ; i++) {
-    tft->fillRect( x + i * (barWidth + barSpace), y, barWidth, barMaxHeight , ILI9341_LIGHTGREY);
-  }
-}
-
 LevelIndicator::LevelIndicator(){
 }
 
@@ -137,6 +91,24 @@ LevelIndicator::LevelIndicator(Adafruit_ILI9341 &dsp, int x0, int y0, int W0, in
   barWidth = W/valueCount - barSpace;
   //barWidth = 10;
   //barSpace = 2;
+
+  font = &FreeSansBold12pt7b;
+  fontSize = 1;
+  tft->setFont(font);
+  tft->setTextSize(fontSize);
+
+  int16_t  txtX, txtY;
+  uint16_t txtW, txtH;
+  //Probe font height for number indicator
+  //Only height is important to align text with level indicator
+  tft->getTextBounds("0123456789", 0, 0, &txtX, &txtY, &txtW, &txtH);
+  fontHeight = txtH;
+
+  int cursorX = x + valueCount*(barWidth + barSpace) + 10;
+  int cursorY = H + y - fontHeight/2;
+  //NumberIndicator(Adafruit_ILI9341 &dsp, int x, int y, GFXfont *font, int size);
+  level_value = NumberIndicator(dsp, cursorX, cursorY, font, fontSize);
+  //level_value = NumberIndicator(dsp);
 
   for (int i = 0; i < valueCount; i++){
     tft->drawRect(x + i*(barWidth + barSpace), y, barWidth, H, ILI9341_WHITE);
@@ -152,18 +124,11 @@ void LevelIndicator::set(float value){
 
   for (int i = 0; i < valueCount; i++){
     if (i < level){
-        tft->fillRect(x + i*(barWidth + barSpace), y, barWidth, H, ILI9341_GREENYELLOW);
+        tft->fillRect(x + i*(barWidth + barSpace), y, barWidth, H, ILI9341_GREEN);
     } else {
         tft->fillRect(x + i*(barWidth + barSpace), y, barWidth, H, ILI9341_BLACK);
         tft->drawRect(x + i*(barWidth + barSpace), y, barWidth, H, ILI9341_WHITE);
     }
   }
-
-  tft->setFont();
-  tft->setTextSize(3);
-  tft->setTextColor(ILI9341_WHITE);
-  tft->setCursor(x + 10 + valueCount*(barWidth + barSpace), y + H/2);
-  tft->print(value, 1);
-  tft->print("m/s");
-
+  level_value.set(value);
 }
