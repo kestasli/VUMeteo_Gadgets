d1cdf872dcc7e473dd46aef4de27b49fa3742839
kestasli <k.liaugminas@gmail.com>
2017-11-24 23:28:47 +0200

    Major changes

--- MeteoDashboard.h
+++ MeteoDashboard.h
@@ -1,27 +1,40 @@
+#define ALIGN_STD     0
+#define ALIGN_H       1
+#define ALIGN_V       2
+#define ALIGN_VH      3
+#define ALIGN_TOPC    4
+
 class NumberIndicator{
 public:
   NumberIndicator();
-  NumberIndicator(Adafruit_ILI9341 &dsp, int x, int y);
-  //Temp(Adafruit_ILI9341 &dsp, int x, int y, GFXfont *font);
-  NumberIndicator(Adafruit_ILI9341 &dsp, int x, int y, const GFXfont *font, int size);
-  void set(float value);
-  void setFormat(int comma, const char *unit, uint16_t color);
+  //function takes pointer to display, x coord, y coord, font, alignment style
+  NumberIndicator(Adafruit_ILI9341 &dsp, int x, int y, const GFXfont *font, int alignment);
+  void set(float value, uint16_t color);
+  void setFormat(int comma, const char *unit);
+  void setFormat(int comma, const char *unit, bool averaging);
 private:
   char* deblank(char* origstring);
   char* addUnits(char* origstring);
+  float getAverage(float value);
   Adafruit_ILI9341 *tft;
   const GFXfont *font;
-  int W, H, x, y; //display width and height
-  char unitIndicator[5] = {0};
-  //char *outputWithUnits;
-  char outputWithUnits[10] = {0};
-  uint16_t color;
+  int W, H; //display width and height
+  int x, y; //coordinates for font placement
+  char unitIndicator[5] = {0}; //this is array for measurement units
+  char outputWithUnits[10] = {0}; //this holds combined array from value and unit
+
+  float measureList[60] = {0}; //average over array
+  int measureNumber = 0; //this holds measurement number for calc average
+
+  uint16_t color = ILI9341_WHITE;
   int decimalPlace = 1;
-  int size = 1; //font size
+  int alignment = ALIGN_STD; //defines how text should be aligned
+  bool averaging = false; //defines how text should be aligned
+  int fontSize = 1; //font size
   float oldvalue = 99.9;
   float value = 0.0;
-  int16_t  txtX, txtY; //current coordinates of text boudary
-  uint16_t txtW, txtH; //current coordinates of text boudary
+  int16_t  txtX, txtY; //current coordinates of text boundary
+  uint16_t txtW, txtH; //current text width and height
   int16_t  o_txtX = 0, o_txtY = 0; //keep old coordinates of text boundary
   uint16_t o_txtW = 0, o_txtH = 0; //keep old koordinates of text boundary
 };
@@ -45,6 +58,29 @@ private:
   int maxvalue = 10;
   int barWidth = 0, barSpace = 4;
   int valueCount = 10; //bar quantity in level indicator
-  float measureList[10] = {0}; //average over array
+  float measureList[15] = {0}; //average over array
   int measureNr = 0;
+};
+
+class DirectionIndicator{
+public:
+  DirectionIndicator();
+  DirectionIndicator(Adafruit_ILI9341 &dsp, int x, int y, int radius, uint16_t color);
+  void set(float value, int direction);
+private:
+    int getAngleAverage(int value);
+    Adafruit_ILI9341 *tft;
+    double radianCoef = 0.0174532925;
+    NumberIndicator indicator;
+    const GFXfont *font;
+    int x, y, radius, direction, old_direction;
+
+    int measureList[10] = {0}; //average over array
+    int measureNumber = 0; //this holds measurement number for calc average
+
+    int old_markerX, old_markerY;
+    int old_markerX1, old_markerY1;
+    int old_markerX2, old_markerY2;
+    float value, old_value;
+    uint16_t color;
 };
--- MeteoDashbord.cpp
+++ MeteoDashbord.cpp
@@ -1,39 +1,32 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_ILI9341.h>
 #include <MeteoDashboard.h>
-#include <Fonts/FreeSansBold18pt7b.h>
+//#include <Fonts/FreeSansBold18pt7b.h>
+#include <math.h>
 //#include <Fonts/FreeMonoBold12pt7b.h>
-//#include <Fonts/FreeSansBold24pt7b.h>
+#include <Fonts/FreeSansBold24pt7b.h>
 //#include <Fonts/FreeSansBold56pt7b.h>
 //#include <Fonts/FreeSansBold44pt7b.h>
 
 NumberIndicator::NumberIndicator(){
 }
 
-NumberIndicator::NumberIndicator(Adafruit_ILI9341 &dsp, int x0, int y0){
-  tft = &dsp;
-  W = tft->width();
-  H = tft->height();
-  x = x0, y = y0;
-  tft->setTextSize(2);
-}
-
-NumberIndicator::NumberIndicator(Adafruit_ILI9341 &dsp, int x0, int y0, const GFXfont *font0, int size0){
+NumberIndicator::NumberIndicator(Adafruit_ILI9341 &dsp, int x0, int y0, const GFXfont *font0, int alignment0){
   tft = &dsp;
   W = tft->width();
   H = tft->height();
   x = x0, y = y0;
   font = font0;
-  size = size0;
+  alignment = alignment0;
   tft->setFont(font);
-  tft->setTextSize(size);
+  tft->setTextSize(fontSize);
 }
 
-void NumberIndicator::set(float value0){
+void NumberIndicator::set(float value0, uint16_t color0){
   tft->setTextWrap(false);
-  tft->setTextColor(ILI9341_WHITE);
+  tft->setTextColor(color0);
   tft->setFont(font);
-  tft->setTextSize(size);
+  tft->setTextSize(fontSize);
   value = value0;
 
   int cursorX = x;
@@ -41,8 +34,13 @@ void NumberIndicator::set(float value0){
 
   //char value_str[] = "-00.0";
   char value_str[6];
-  //if current value is the same as old- do nothing
 
+  //if set to TRUE takes range of values and average
+  if(averaging){
+    value = getAverage(value);
+  }
+
+  //if current value is the same as old- do nothing
   if(oldvalue != value){
     //clean old text by placing black rect over old coordinates
     tft->fillRect(o_txtX, o_txtY, o_txtW, o_txtH, ILI9341_BLACK);
@@ -53,10 +51,20 @@ void NumberIndicator::set(float value0){
     tft->getTextBounds(valueWithUits, 0, 0, &txtX, &txtY, &txtW, &txtH);
 
     //If both coordinates set to 0 then place authomatically top/center
-    if ((x == 0) & (y == 0)){
+    if (alignment == ALIGN_TOPC){
       cursorX = (W - txtW)/2;
       cursorY = txtH;
     }
+
+    if (alignment == ALIGN_STD){
+      cursorX = x, cursorY = y;
+    }
+
+    if (alignment == ALIGN_VH){
+      cursorX = x - txtW/2;
+      cursorY = y + txtH/2;
+    }
+
 
     tft->setCursor(cursorX, cursorY);
     tft->print(valueWithUits);
@@ -66,12 +74,62 @@ void NumberIndicator::set(float value0){
   oldvalue = value;
 }
 
-void NumberIndicator::setFormat(int decimalPlace0, const char *unitIndicator0, uint16_t color0){
+void NumberIndicator::setFormat(int decimalPlace0, const char *unitIndicator0){
   decimalPlace = decimalPlace0;
-  color = color0;
   for (unsigned int i = 0; i < strlen(unitIndicator0); i++){
     unitIndicator[i] = unitIndicator0[i];
   }
+}
+
+void NumberIndicator::setFormat(int decimalPlace0, const char *unitIndicator0, bool averaging0){
+  decimalPlace = decimalPlace0;
+  averaging = averaging0;
+  averaging = averaging0;
+  for (unsigned int i = 0; i < strlen(unitIndicator0); i++){
+    unitIndicator[i] = unitIndicator0[i];
+  }
+}
+
+
+float NumberIndicator::getAverage(float value){
+  int size = sizeof(measureList)/sizeof(float);
+
+  measureNumber++;
+  if (measureNumber > size){
+    measureNumber = size;
+  }
+
+  for (int i = size - 1 ; i > 0 ; i--){
+    measureList[i] = measureList[i - 1];
+  }
+
+  measureList[0] = value;
+  float measureSum = 0;
+
+  for (int i = 0; i < measureNumber ; i++){
+    measureSum = measureSum + measureList[i];
+  }
+
+  float average = measureSum/(float)measureNumber;
+
+  //it is needed to remove unsignificant decimals
+  //as indicator refreshes only if prev value differs from current
+  //this can cause indicator refresh every time as two floats will never be the same
+
+  average = ((int)(average * 10 ))/ 10.0;
+  /*
+  for (int i = 0; i < size; i++){
+    Serial.print(measureList[i]);
+    Serial.print(", ");
+  }
+  */
+  Serial.print("AVG: ");
+  Serial.print(average);
+  Serial.print(", ");
+  Serial.print(measureNumber);
+  Serial.println("");
+
+  return average;
 }
 
 char* NumberIndicator::addUnits(char* input)
@@ -88,7 +146,7 @@ char* NumberIndicator::addUnits(char* input)
     }
 
     output[j]=0;
-    sprintf(outputWithUnits, "%s %s", output, unitIndicator);
+    sprintf(outputWithUnits, "%s%s", output, unitIndicator);
     //Serial.println(unitIndicator);
     return outputWithUnits;
 }
@@ -102,7 +160,7 @@ LevelIndicator::LevelIndicator(Adafruit_ILI9341 &dsp, int x0, int y0, int W0, in
   tft = &dsp;
   barWidth = W/valueCount - barSpace;
 
-  font = &FreeSansBold18pt7b;
+  font = &FreeSansBold24pt7b;
   fontSize = 1;
   tft->setFont(font);
   tft->setTextSize(fontSize);
@@ -139,7 +197,7 @@ void LevelIndicator::set(float value){
     }
   }
 
-  level_value.set(value);
+  level_value.set(value, ILI9341_WHITE);
 }
 
 void LevelIndicator::setAvg(float value){
@@ -183,15 +241,124 @@ void LevelIndicator::setAvg(float value){
     }
   }
 
-  level_value.set(value);
+  level_value.set(value, ILI9341_WHITE);
 }
 
 void LevelIndicator::setFormat(int decimalPlace0, const char *unitIndicator0, uint16_t color0){
     decimalPlace = decimalPlace0;
     color = color0;
-    //unitIndicator = unitIndicator0;
+    //copy values from parameter to class local variables
     for (int i = 0; i < strlen(unitIndicator0); i++){
       unitIndicator[i] = unitIndicator0[i];
     }
-    level_value.setFormat(decimalPlace, unitIndicator, color);
+    level_value.setFormat(decimalPlace, unitIndicator);
+}
+
+DirectionIndicator::DirectionIndicator(){
+}
+
+DirectionIndicator::DirectionIndicator(Adafruit_ILI9341 &dsp, int x0, int y0, int r0, uint16_t color0){
+  x = x0, y = y0, radius = r0, color = color0;
+  tft = &dsp;
+  int W = tft->width();
+  int H = tft->height();
+  font = &FreeSansBold24pt7b;
+
+  if (x == 0){x = W/2;}
+  if (y == 0){y = H/2;}
+
+  //tft->drawCircle(x, y, radius, color);
+  //tft->drawCircle(x, y, radius + 1, color);
+  indicator = NumberIndicator(dsp, x, y, font, ALIGN_VH);
+  indicator.setFormat(1, "", true);
+}
+
+void DirectionIndicator::set(float value0, int direction0){
+  //calculate new marker position
+  int markerLenght = radius * 1.2;
+  int markerExt = radius * 1.1;
+  int markerInt = radius * 0.7;
+  //marker widt, degrees
+  int markerWidth = 8;
+  value = value0;
+
+  int direction = getAngleAverage(direction0);
+  direction = -direction + 180;
+  //calc triangle coordinates
+
+  int markerX = x + markerInt * sin(direction * radianCoef);
+  int markerY = y + markerInt * cos(direction * radianCoef);
+  int markerX1 = x + markerExt * sin((direction + markerWidth) * radianCoef);
+  int markerY1 = y + markerExt * cos((direction + markerWidth) * radianCoef);
+  int markerX2 = x + markerExt * sin((direction - markerWidth) * radianCoef);
+  int markerY2 = y + markerExt * cos((direction - markerWidth) * radianCoef);
+
+  //remove old marker
+  tft->fillTriangle(old_markerX1, old_markerY1, old_markerX2, old_markerY2, old_markerX, old_markerY, ILI9341_BLACK);
+  tft->drawCircle(x, y, radius, color);
+  tft->drawCircle(x, y, radius - 1, color);
+  tft->fillTriangle(markerX1, markerY1, markerX2, markerY2, markerX, markerY, ILI9341_RED);
+
+  indicator.set(value, ILI9341_LIGHTBLUE);
+
+  old_markerX = markerX;
+  old_markerY = markerY;
+  old_markerX1 = markerX1;
+  old_markerY1 = markerY1;
+  old_markerX2 = markerX2;
+  old_markerY2 = markerY2;
+}
+
+int DirectionIndicator::getAngleAverage(int value){
+
+    int size = sizeof(measureList)/sizeof(int);
+
+    measureNumber++;
+    if (measureNumber > size){
+      measureNumber = size;
+    }
+
+    for (int i = size - 1 ; i > 0 ; i--){
+      measureList[i] = measureList[i - 1];
+    }
+
+    measureList[0] = value;
+
+    float s = 0.0;
+    float c = 0.0;
+
+    for (int i = 0; i < measureNumber ; i++){
+      s = s + sin(radianCoef * measureList[i]);
+      c = c + cos(radianCoef * measureList[i]);
+      //measureSum = measureSum + measureList[i];
+    }
+
+    Serial.println(s);
+    Serial.println(c);
+
+    s = s/(float)measureNumber;
+    c = c/(float)measureNumber;
+
+    float average;
+
+    average = atan2(s, c)/radianCoef;
+
+
+    //it is needed to remove unsignificant decimals
+    //as indicator refreshes only if prev value differs from current
+    //this can cause indicator refresh every time as two floats will never be the same
+
+
+    for (int i = 0; i < size; i++){
+      Serial.print(measureList[i]);
+      Serial.print(", ");
+    }
+
+    Serial.print("AVG: ");
+    Serial.print(average);
+    Serial.print(", ");
+    Serial.print(measureNumber);
+    Serial.println("");
+
+    return (int)average;
 }
